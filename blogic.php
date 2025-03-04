4e9d87c922f02494eb772b6f9f1e534593cb25e4
mohamed mustafa <147065419+mohamed8112001@users.noreply.github.com>
2025-03-04 16:42:32 +0200

    blogic.php

--- blogic.php
+++ blogic.php
@@ -2,55 +2,71 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-
+// include_once('mysqlipro.php');
+// include_once('mysqlioop.php');
 include_once('database.php');
-// // include_once('config.php'); 
 
-// function getAllRooms()
-// {
-//     connecttodb();
-//     $res = myselect('room');
-//     return $res ;
-// }
-
-// function getallintakes()
-// {
-//     // //pdo
-//     $res=mySelect('intake');
-//     return $res;
-// }
-
-class User extends Database {
-
-    public function getAllRooms()
-    {
+function getallintakes()
+{
     // //pdo
-        // $res=mySelect('intake');
-        return $this->select('room');
-    }
-
-
-    public function getAllIntakes()
-    {   //pdo
-        // $res=mySelect('trainee');
-        return $this->select('intake');
-    }
-
-    public function insertAllTrainees($name,$email,$password,$intake,$profile_picture)
-    {
-        $res = this->insert('trainee',['username','email','password','intake_id','image'],[$name,$email,$password,$intake,$profile_picture]);
-    }
+    $res=mySelect('intake');
+    return $res;
+}
+function getalltrainees()
+{   //pdo
+    $res=mySelect('trainee');
+    return $res;
 }
 
-// function insert($name,$email,$password,$intake,$profile_picture)
-// {
-//     connecttodb();
+// function deleteUser() {
+//     $id = $_GET['user_id'] ?? null;
 
-//     $Res = myInsert('trainee',['username','email','password','intake_id','image']
-//     ,[$name,$email,$password,$intake,$profile_picture]);
+//     if ($id) {
+//         $deletedRows = mydelete("trainee", "user_id = :id", [":id" => $id]);  // Fix binding issue
+//         if ($deletedRows) {
+//             header("Location: AllUsers.php"); 
+//             exit; // Stop further execution
+//         } else {
+//             echo "Error deleting user.";
+//         }
+//     } else {
+//         echo "No user ID provided.";
+//     }
+// }
+
+// function trainevalis($email)
+// {
     
 // }
 
+// function inserttrainee( $username, $email, $password, $intake_id,$image) {
+//     $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+//     $stmt = $conn->prepare("INSERT INTO users (username, email, password,intake_id, image) VALUES (?, ?, ?, ?, ?)");
+//     return $stmt->execute([$username, $email, $hashedPassword,$intake_id,$image]);
+// }
+function inserttrainee($username,$email,$password,$intake_id,$image)
+{
+    $res=myinsert('trainee',['username','email','password','intake_id','profile_image'],
+    [$username,$email,$password,$intake_id,$image]);
+    
+    if($res>0)
+    {return '<div><h1>customer added</h1></div>';}
+    else
+    {return '<div><h1>Error while Customer adding </h1></div>';}
+}
 
-?>
 
+// class User extends Database {
+
+//     public function getallintakes()
+//     {
+//     // //pdo
+//         // $res=mySelect('intake');
+//         return $this->select('room');
+//     }
+//     public function getalltrainees()
+//     {   //pdo
+//         // $res=mySelect('trainee');
+//         return $this->select('trainee');
+//     }
+// }
